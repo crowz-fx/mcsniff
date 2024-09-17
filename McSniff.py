230d1040c1cd0c6e9015d07728c5355cf77a80ac
Lui Crowie <22945441+crowz-fx@users.noreply.github.com>
2024-09-17 17:46:56 +0100

    initial parsing of eth frame, updated README

--- McSniff.py
+++ McSniff.py
@@ -1,4 +1,43 @@
-from scapy.all import sniff
+"""
 
+    McSniff - Lui Crowie, 2024
 
-sniff(iface="eth0", prn=lambda x: x.show())
+"""
+
+import socket, sys
+import struct
+
+# create the actual socket connection and bind to an interface
+try:
+    """
+     - socket info - https://www.man7.org/linux/man-pages/man2/socket.2.html
+     - family - 17, low-level packet interface
+     - type -  3, says on the tin, raw protocol access
+     - protocol - ETH_P_ALL, give us every packet!
+    """
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+
+    # bind to a specific interface
+    s.bind(("eth0", 0))
+except socket.error as error:
+    print(
+        f"Failed to create socket connection, error=[{str(error.errno)}], message=[{error.strerror}]"
+    )
+    sys.exit()
+
+# loop and process each packet recieved
+while True:
+    # more than max packet size, even if eth frames are much lower
+    # tuple = (bytes, return address), pull raw data
+    unprocessed_data, address = s.recvfrom(65565)
+
+    # 14 = dest mac (6), src mac (6), ethertype/len (2)
+    frame_header_len = 14
+    frame_header = struct.unpack("!6s6s2s", unprocessed_data[:frame_header_len])
+    
+    print(frame_header)
+
+    src_mac, dest_mac, ethertype = frame_header
+
+    print(src_mac, dest_mac, ethertype)
+
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mcsniff
 packet sniffer and interpreter... you know for research purposes
+
+### Reasoning
+I wanted to literally dig into the payloads, frames, packets etc. of what's actually going back and forth over a network. Understanding the breakdown of the structure, byte-for-byte was a good learning experience. 
+
+I know, I know, I can just use scapy or use WireShark and dig into them, but where's the fun in that!?
+
+### Notes 
+1. Example below of how to do the same in scapy
+2. For a deep dive into ethernet frames, see [this great resource](https://www.freecodecamp.org/news/the-complete-guide-to-the-ethernet-protocol/)
+
+## Features and functionality
+1. Ethernet frame processing
+2. ...?
+
+## Run
+```bash
+sudo python McSniff.py
+```
+
+## Example from scapy
+```python
+from scapy.all import sniff
+sniff(iface="eth0", prn=lambda x: x.show())
+```
